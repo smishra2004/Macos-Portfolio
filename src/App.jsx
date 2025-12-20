be33037ec3e4e050ac390c8792f4def55406d950
Shubham Mishra <shubham.smishra2004@gmail.com>
2025-12-20 14:19:49 +0530

    added all features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Navbar, Welcome, Dock } from '#components'
 import gsap from "gsap";
 import { Draggable } from 'gsap/Draggable'
-import { Terminal } from '#windows';
+import {Terminal, Resume, Safari, Finder, Text, Image, Contact } from '#windows';
 gsap.registerPlugin(Draggable);
 import React from 'react'
 
@@ -12,6 +12,12 @@ const App = () => {
       <Welcome />
       <Dock />
       <Terminal />
+      <Safari />
+      <Resume />
+      <Finder />
+      <Text />
+      <Image />
+      <Contact />
     </main>
   )
 }
---
+++ src/windows/Contact.jsx
@@ -0,0 +1,36 @@
+import WindowWrapper from '#hoc/WindowWrapper'
+import { socials } from '#constants'
+import React from 'react'
+import { WindowControls } from '#components'
+
+const Contact = () => {
+  return (
+    <>
+        <div id='window-header'>
+            <WindowControls target="contact" />
+            <h2>Contact me</h2>
+        </div>
+
+        <div className='p-5 space-y-5'>
+            <img src="/images/adrian.jpg" alt="shubham" className='w-20 rounded-full' />
+            <h3>Let's Connect</h3>
+            <p>Got an idea? A bug to fix or just wanna talk code? I'm in</p>
+            <p>shubham.smishra2004@gmail.com</p>
+
+            <ul>
+                {socials.map(({id,bg,link,icon,text})=>(
+                    <li key={id} style={{backgroundColor: bg}} >
+                        <a href={link} target='_blank' rel="noopener noreferrer" title={text} >
+                            <img src={icon} alt={text} className='size-5' />
+                            <p>{text}</p>
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </>
+  )
+}
+
+const ContactWindow = WindowWrapper(Contact,"contact")
+export default ContactWindow
